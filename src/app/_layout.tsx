a6a26d4a374dac5532bca9d1e140752a4e8696ab
bqdiu <boquangdieu2003@gmail.com>
2024-06-23 18:46:13 +0700

    fix Auth user side

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -54,8 +54,8 @@ function RootLayoutNav() {
       <AuthProvider>
         <CartProvider>
           <Stack>
-            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
             <Stack.Screen name="(admin)" options={{ headerShown: false }} />
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
             <Stack.Screen name="(user)" options={{ headerShown: false }} />
             <Stack.Screen name="cart" options={{ presentation: 'modal' }} />
           </Stack>
