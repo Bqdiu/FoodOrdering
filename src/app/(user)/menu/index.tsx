dff39e237423a73275bc797160e519f0db11f41e
bqdiu <boquangdieu2003@gmail.com>
2024-06-25 17:51:45 +0700

    fetch products by react query

--- src/app/(user)/menu/index.tsx
+++ src/app/(user)/menu/index.tsx
@@ -1,18 +1,39 @@
-import { FlatList } from 'react-native';
-import products from 'assets/data/products';
+import { ActivityIndicator, FlatList } from 'react-native';
 import ProductListItem from '@/src/components/ProductListItem';
-
+import { supabase } from '@/src/lib/supabase';
+import { useQuery } from '@tanstack/react-query';
+import { Text } from 'react-native';
 export default function MenuScreen() {
   const numColumns = 2;
+
+  const { data: products, error, isLoading } = useQuery({
+    queryKey: ['products'],
+    queryFn: async () => {
+      const { data, error } = await supabase.from('products').select('*');
+      if (error) {
+        throw new Error(error.message);
+      }
+      return data;
+    }
+  })
+
+  if(isLoading){
+    return <ActivityIndicator />
+  }
+
+  if(error){
+    return <Text>Failed to fetch products</Text>
+  }
+  
   return (
-      <FlatList 
-        data = {products}
-        renderItem={({item}) => <ProductListItem product={item} />}
-        numColumns={numColumns}
-        key={numColumns}
-        contentContainerStyle={{ gap: 10 , padding: 10 }}
-        columnWrapperStyle={{ gap: 10 }}
-      />
+    <FlatList
+      data={products}
+      renderItem={({ item }) => <ProductListItem product={item} />}
+      numColumns={numColumns}
+      key={numColumns}
+      contentContainerStyle={{ gap: 10, padding: 10 }}
+      columnWrapperStyle={{ gap: 10 }}
+    />
   );
 }
 
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -8,7 +8,7 @@ import 'react-native-reanimated';
 import { useColorScheme } from '@/src/components/useColorScheme';
 import CartProvider from '../providers/CartProvider';
 import AuthProvider from '../providers/AuthProvider';
-
+import QueryProvider from '../providers/QueryProvider';
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
@@ -52,14 +52,16 @@ function RootLayoutNav() {
   return (
     <ThemeProvider value={colorScheme === 'light' ? DarkTheme : DefaultTheme}>
       <AuthProvider>
-        <CartProvider>
-          <Stack>
-            <Stack.Screen name="(admin)" options={{ headerShown: false }} />
-            <Stack.Screen name="(user)" options={{ headerShown: false }} />
-            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-            <Stack.Screen name="cart" options={{ presentation: 'modal' }} />
-          </Stack>
-        </CartProvider>
+        <QueryProvider>
+          <CartProvider>
+            <Stack>
+              <Stack.Screen name="(admin)" options={{ headerShown: false }} />
+              <Stack.Screen name="(user)" options={{ headerShown: false }} />
+              <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+              <Stack.Screen name="cart" options={{ presentation: 'modal' }} />
+            </Stack>
+          </CartProvider>
+        </QueryProvider>
       </AuthProvider>
     </ThemeProvider>
   );
